7bb2bf3b04a059c85c29a1762d9f51e911a24d4c
dd00697 <dd00697@surrey.ac.uk>
2025-12-01 18:16:09 +0000

    Added config  with paths and labels

--- ml/config.py
+++ ml/config.py
@@ -1,11 +1,30 @@
-CONFIG = {
-    "experiment_name": "exp_v1_baseline",
-    "arch": "tf_efficientnet_b4_ns",
-    "n_folds": 5,
-    "img_size": 448,
-    "frozen_epochs": 5,
-    "finetune_epochs": 15,
-    "lr": 1e-3,
-    "lr_max": 1e-3,
-    "batch_size": 64,
-}
+from pathlib import Path
+
+# Root of the repo
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+
+#Data Paths
+DATA_DIR = ROOT_DIR / "data" / "plant"
+IMAGES_DIR = DATA_DIR / "images"
+TRAIN_CSV = DATA_DIR / "train.csv"
+TEST_CSV = DATA_DIR / "test.csv"
+
+# Data Labels
+TARGET_COLS = ['healthy', 'multiple_diseases', 'rust', 'scab']
+
+# Model Paths
+MODELS_DIR = ROOT_DIR / "models"
+
+BEST_MODEL_NAME = "tf_efficientnet_b4_ns_fold0_best.pkl"
+BEST_MODEL_PATH = MODELS_DIR / BEST_MODEL_NAME
+
+# Training Config
+
+N_FOLDS = 5
+IMG_SIZE = 380
+BATCH_SIZE = 32
+EPOCHS = 5
+LR = 2e-3
+ARCH = "tf_efficientnet_b4_ns"
+
