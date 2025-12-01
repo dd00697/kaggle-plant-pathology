94c3392b1a373658cf747c823f4c2783f43a7c98
dd00697 <dd00697@surrey.ac.uk>
2025-12-01 20:29:43 +0000

    added inference wrapper using exported trained model

---
+++ ml/inference.py
@@ -0,0 +1,37 @@
+import io
+from typing import Dict, Any
+import torch
+from fastai.vision.all import load_learner, PILImage
+from .config import BEST_MODEL_PATH
+
+print(f"Loading model from {BEST_MODEL_PATH}")
+learn = load_learner(BEST_MODEL_PATH)
+learn.model.eval()
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    learn.dls = learn.dls.cuda()
+    learn.model.to(device)
+    DEVICE = "cuda"
+else:
+    DEVICE = "cpu"
+
+
+def predict_image_bytes(image_bytes: bytes) -> Dict[str, Any]:
+    #Take raw image byes and return, predicted label and the probabilties per class
+
+    img = PILImage.create(io.BytesIO(image_bytes))
+
+    pred_class, pred_idx, probs = learn.predict(img)
+
+    classes = list(map(str, learn.dls.vocab))
+    probs_list = [float(p) for p in probs]
+
+    prob_dict = dict(zip(classes, probs_list))
+
+    return {
+        "predicted_label" : str(pred_class),
+        "predicted_index" : int(pred_idx),
+        "probabilities" : prob_dict,
+        "device" : DEVICE,
+    }
