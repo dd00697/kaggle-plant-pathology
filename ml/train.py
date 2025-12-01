053043093f4114f8aed0df531b83d14bfb3f62a0
dd00697 <dd00697@surrey.ac.uk>
2025-12-01 18:35:52 +0000

    Add training script

--- ml/train.py
+++ ml/train.py
@@ -1,10 +1,56 @@
-from ml.config import CONFIG
+from pathlib import Path
+import torch
+from fast.ai.vision.all import *
+from fastai.callback.tracker import SaveModelCallback
+from.config import (
+    MODELS_DIR, ARCH, N_FOLDS, EPOCHS, LR
+)
+from .data import load_train_df, add_folds, get_dols_for_fold
 
-def main():
-    print("Training with config:")
-    for k, v in CONFIG.items():
-        print(f"  {k}: {v}")
-    # TODO: later: import get_dls_for_fold, create_learner, etc.
 
-if __name__ == "__main__":
-    main()
+def train_single_fold(fold: int) -> Path:
+    print(f"==== Training fold {fold} ====")
+
+    train_df = load_train_df()
+    train_df = add_folds(train_df)
+
+    dls = get_dols_for_fold(train_df, fold)
+
+    learn = timm_vision__learner(
+        dls, 
+        ARCH,
+        metrics=accuracy,
+        pretrained=True,
+    )
+
+    cbs = [
+        SaveModelCallback(
+            monitor='accuracy',
+            fname=f'{ARCH}_fold{fold}_best'
+        )
+    ]
+
+    if torch.cuda.is_available():
+        learn.to_fp16()
+        print("Using GPU")
+
+    learn.fine_tune(EPOCHS, base_lr=LR, cbs=cbs)
+
+    export_path = MODELS_DIR / f'{ARCH}_fold{fold}_best.pkl'
+    learn.export(export_path)
+    print(f"Exported fold {fold} model to {export_path}")
+
+    return export_path
+
+
+def train_all_folds():
+    paths = []
+    for fold in range(N_FOLDS):
+        paths.append(train_single_fold(fold))
+    print("Done training all folds: ")
+    for p in paths:
+        print(" -", p)
+
+    if __name__ == "__main__":
+        MODELS_DIR.mkdir(parents=True, exist_ok=True)
+        train_all_folds()
